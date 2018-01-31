618b4c7623ae6bc28a00f79ba64b131445b6887d
tg0825 <tg0825@backpack.kr>
2018-02-01 01:33:47 +0900

    redux 연습 중

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './components/App';
+import {createStore} from 'redux';
+import reducers from './reducers';
+
+const store = createStore(reducers);
 
 import { createStore } from 'redux';
 import reducers from './reducers';
